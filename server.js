8a9549e480dcd27ed98ac353a0ec6a85635e0c45
Xis <rick-s@hotmail.com>
2021-01-21 10:28:34 -0800

    server setup

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,8 +13,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactCRM");
 // Define API routes here
 
 // Send every other request to the React app
