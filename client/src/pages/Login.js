0cc82861a626355887557506a0d54bbd3e95e53b
Danielle Cutler <cutlerd02@gmail.com>
2021-01-21 15:18:09 -0700

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/header";
+import { Dropdown } from "react-bootstrap";
 
 function Login() {
-    return (
-        <div>
-            <h1> We Did it </h1>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <h1> We Did it </h1>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
