9f461b5f00150d75f0694b4b609114b6f6c2a994
Danielle Cutler <cutlerd02@gmail.com>
2021-01-22 12:51:57 -0700

    userprofilestyle

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/Login"
-import Home from "./pages/Home"
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 import { StoreProvider } from "./utils/GlobalState";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <Router>
-    <div>
-      <StoreProvider>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/home" component={Home} />
-        </Switch>
-      </StoreProvider>
-    </div>
-  </Router>
+      <div>
+        <StoreProvider>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/UserProfile" component={UserProfile}></Route>
+          </Switch>
+        </StoreProvider>
+      </div>
+    </Router>
   );
 }
-
 
 export default App;
--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -5,39 +5,20 @@ import Button from "react-bootstrap/Button";
 
 function UserProfile({ name, jobTitle, userPic }) {
   return (
-    <Card
-      style={{
-        width: "33vw",
-        height: "100vh",
-        border: "3px solid grey",
-        marginLeft: "15px",
-      }}
-    >
+    <Card className="user-profile-card">
       <Card.Img
+        className="user-image"
         variant="top"
         src="https://avatars.githubusercontent.com/u/72281065?s=460&u=09e78bf1d0aa1708deb3a7d9f5cab7a5ed5b012d&v=4"
-        style={{
-          margin: "auto",
-          marginTop: "30px",
-          border: "10px solid grey",
-          borderRadius: "50%",
-          width: "90%",
-        }}
       />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>{jobTitle}</Card.Text>
       </Card.Body>
       <ButtonGroup vertical>
-        <Button style={{
-            height: "5rem"
-        }}>Tasks</Button>
-        <Button style={{
-            height: "5rem"
-        }}>Leads</Button>
-        <Button style={{
-            height: "5rem"
-        }}>E-Mail</Button>
+        <Button className="user-button">Tasks</Button>
+        <Button className="user-button">Leads</Button>
+        <Button className="user-button">E-Mail</Button>
       </ButtonGroup>
       <Card.Body>
         <Card.Link href="#">Card Link</Card.Link>
