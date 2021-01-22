0839ea7d3d4e51b76ab137a2497d0c81f126278d
Xis <rick-s@hotmail.com>
2021-01-21 20:04:31 -0800

    checking out

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyCamNLHUP3uDwUAN80Xtx_1hVNdfFYBebY",
+  authDomain: "cb-simple-react-app.firebaseapp.com",
+  databaseURL: "https://cb-simple-react-app.firebaseio.com",
+  projectId: "cb-simple-react-app",
+  storageBucket: "cb-simple-react-app.appspot.com",
+  messagingSenderId: "425786567074",
+  appId: "1:425786567074:web:b8074db6af5e2886",
+};
+let app = firebase.initializeApp(firebaseConfig);
+let db = app.firestore();
+
+export function auth() {
+  return app.auth();
+}
+
+export default db;
