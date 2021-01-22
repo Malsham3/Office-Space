c1c6ce0bdf65859123edcbbbd12e0a56f52322e0
Danielle Cutler <cutlerd02@gmail.com>
2021-01-22 10:10:44 -0700

    stupidheader

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Header from '../components/Header'
-import LoginForm from '../components/LoginForm'
+import React from "react";
+import Header from "../components/Header";
+import LoginForm from "../components/LoginForm";
 
 function Login() {
-    return (
-        <>
-            <Header title="Office Space" tag="I'll burn the building down"/>
-            <LoginForm />
-        </>
-    )
+  return (
+    <>
+      <Header title="Office Space" tag="I'll burn the building down" />
+      <LoginForm />
+    </>
+  );
 }
 
 export default Login;
