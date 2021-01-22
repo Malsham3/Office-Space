65b0db12715e7883730e5356110d0410d2e3698d
Mostafa <m.alsham627@gmail.com>
2021-01-21 20:33:10 -0700

    login component + styling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Form, Button, Card, Container } from "react-bootstrap";
+import "./Style.css";
 
 function LoginForm() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Container className="login-container">
+      <Card style = {
+          {borderRadius: "25px", boxShadow: "0 0 50px black"
+          }} className="login-card">
+        <Form className = "login-form">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Card>
+    </Container>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
