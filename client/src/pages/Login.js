9b6a3cf8701e3cfbd8ce1a7c1d71c075e0987580
Xis <rick-s@hotmail.com>
2021-01-21 18:30:35 -0800

    switching to homepage

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import LoginForm from '../components/LoginForm'
 
 function Login() {
     return (
         <>
             <Header title="Office Space" tag="I'll burn the building down"/>
+            <LoginForm />
         </>
     )
 }
