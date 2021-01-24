68fbdb42009f586d7365dece795f653791db684e
Mostafa <m.alsham627@gmail.com>
2021-01-23 19:18:17 -0700

    dynamic task component

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,1 +1,17 @@
+import React from "react";
 
+function Task({tasks}) {
+  return (
+    // dynamically generate task row with a check box / button. 
+    <>
+    {tasks.map((task) => (
+    <tr key={task.id}>
+      <td>{task.content}</td>
+      <td>Button that marks task complete.</td>
+    </tr>
+    ))}
+    </>
+  );
+}
+
+export default Task;
