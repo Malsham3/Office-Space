ddf7a1714e4943213c044358a91791abe931877c
Xis <rick-s@hotmail.com>
2021-01-24 18:12:04 -0800

    fixed footer placement

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,30 @@
 import React from "react";
-import "./Style.css"
+import "./Style.css";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 
-function Header({title, tag}) {
-  
+function Header({ title, tag }) {
   return (
     <>
-      <Jumbotron className = "header-Jumbotron"
+      <Jumbotron
+        className="header-Jumbotron"
         style={{
           backgroundColor: "white",
           display: "flex",
+          justifyContent: "space-between",
           alignItems: "center",
           height: "100px",
           marginBottom: "0px",
+          padding: "0",
         }}
       >
-        <Container className = "header-container" style={{ marginLeft: "10px" }}>
+        <Container className="header-container">
           <h1 style={{ fontWeight: "bold" }}>{title}</h1>
-          <p style = {{ fontSize: "17px"}}>{tag}.</p>
-          <div><a href="/signout">Sign Out</a></div>
+          <p style={{ fontSize: "17px" }}>{tag}.</p>
         </Container>
+        <div className="signout-anchor mr-3 ">
+          <a href="/signout">Sign Out</a>
+        </div>
       </Jumbotron>
     </>
   );
