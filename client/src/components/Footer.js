3d7c7c36dee4ee6034448baff3b935c9dc849850
Mostafa <m.alsham627@gmail.com>
2021-01-25 23:00:04 -0700

    more footer work

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,21 +3,16 @@ import "./Style.css";
 
 function Footer() {
   return (
-    <div className="footer-div">
-    <footer
-      className="footer-2 py-2"
-      style={{ position: "absolute", bottom: "0" }}
-    >
+  
       <div className="container footer-container">
-        <div className="row justify-content-between">
-          <div className="col-12">
+        <div className="row footer-div justify-content-between">
+          <div className="col-10">
             <span className="text-muted">
               Made with
               <span role="img" aria-label="Heart Emoji">
                 ♥️
               </span>{" "}
-              by Rick Smart, Danielle Cutler, Mostafa Alshammary, Bradley
-              Campbell.
+              by Rick Smart, Danielle Cutler, Mostafa Alshammary.
               <br />
               &copy; {new Date().getFullYear()}
             </span>
@@ -33,8 +28,6 @@ function Footer() {
           </div>
         </div>
       </div>
-    </footer>
-    </div>
   );
 }
 
