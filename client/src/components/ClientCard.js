29f48c2b3b5b048abe4f911ea55db2fc2a8e6169
Mostafa <m.alsham627@gmail.com>
2021-01-26 13:32:52 -0700

    leads Accordion

--- client/src/components/ClientCard.js
+++ client/src/components/ClientCard.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Card, ListGroup, ListGroupItem, Image } from "react-bootstrap";
 import "./Style.css";
 import { useStoreContext } from "../utils/GlobalState";
 import API from "../utils/API";
-function ClientCard({clients}) {
+
+function ClientCard({ clients }) {
   const [globalState, dispatch] = useStoreContext();
   // Below array of clients is just an illustration for dynamically generated profile cards for each client. Use Global state or somethin'.s
 
@@ -31,7 +32,18 @@ function ClientCard({clients}) {
     //   For each client, create a card with their information
     <>
       {clients.map((client) => (
-        <h1>client.name</h1>
+        <div className="row">
+          <ul className="col-8">
+            <li>Email: {client.email}</li>
+            <li>Phone Number: {client.phone}</li>
+          </ul>
+
+          <div className="col-2">
+            <Image style = {{}}>
+              
+            </Image>
+          </div>
+        </div>
       ))}
     </>
   );
