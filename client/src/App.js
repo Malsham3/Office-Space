b9b25144dd9cefaca0dc22dd5374872644cda1e9
Xis <rick-s@hotmail.com>
2021-01-27 12:33:03 -0800

    added user to global state

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import SignOut from "./components/SignOut";
-import { StoreProvider } from "./utils/GlobalState";
+import { auth } from "./firebase";
+import Signup from "./pages/Signup";
+import { useStoreContext } from "./utils/GlobalState";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <StoreProvider>
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route exact path="/signout" component={SignOut} />
-          </Switch>
-        </StoreProvider>
-      </Router>
-    </>
-  );
+  const [globalState, dispatch] = useStoreContext();
+
+  const userEmail = auth().onAuthStateChanged((user) => {
+    return user.email;
+  });
+  useEffect(() => {
+    dispatch({
+      type: "SET_USER",
+      payload: userEmail,
+    });
+  }, []);
+
+  if (!globalState.user) {
+    return <Route path="/home" component={Signup} />;
+  } else {
+    return (
+      <>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route exact path="/signout" component={SignOut} />
+        </Switch>
+      </>
+    );
+  }
+
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,15 @@
-import React, {useEffect} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { auth } from "./firebase";
-import Signup from "./pages/Signup";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { StoreProvider } from "./utils/GlobalState";
 
-
-auth().onAuthStateChanged((user) => {
-  // console.log(user)
-  if (!user) {
-    ReactDOM.render(<Signup />, document.getElementById("root"));
-  } else {
-    ReactDOM.render(<App />, document.getElementById("root"));
-  }
-});
-
-
+ReactDOM.render(
+  <Router>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </Router>,
+  document.getElementById("root")
+);
